edca40d2b339ba29a31880ad069baf56ef26346d
oldcoronavirus <manjarolinux123@gmail.com>
2022-10-13 12:18:06 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,143 @@
+import nltk
+nltk.download('popular')
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import pickle
+import numpy as np
+
+from keras.models import load_model
+model = load_model('model.h5')
+import json
+import random
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('texts.pkl','rb'))
+classes = pickle.load(open('labels.pkl','rb'))
+
+def clean_up_sentence(sentence):
+    # tokenize the pattern - split words into array
+    sentence_words = nltk.word_tokenize(sentence)
+    # stem each word - create short form for word
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+
+def bow(sentence, words, show_details=True):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0]*len(words)  
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s: 
+                # assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
+
+def predict_class(sentence, model):
+    # filter out predictions below a threshold
+    p = bow(sentence, words,show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+
+def chatbot_response(msg):
+    ints = predict_class(msg, model)
+    res = getResponse(ints, intents)
+    return res
+
+
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+app.secret_key = 'your secret key'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'user_info'
+
+mysql = MySQL(app)
+
+@app.route('/')
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+	msg = ''
+
+	if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+		username = request.form['username']
+		password = request.form['password']
+		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+		cursor.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, ))
+		account = cursor.fetchone()
+
+		if account:
+			session['loggedin'] = True
+			session['id'] = account['id']
+			session['username'] = account['username']
+			msg = 'Logged in successfully !'
+			return render_template('index.html', msg = msg)
+		else:
+			msg = 'Incorrect username / password !'
+	return render_template('login.html', msg = msg)
+
+@app.route('/logout')
+def logout():
+	session.pop('loggedin', None)
+	session.pop('id', None)
+	session.pop('username', None)
+	return redirect(url_for('login'))
+
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+	msg = ''
+	if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+		username = request.form['username']
+		password = request.form['password']
+		email = request.form['email']
+		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+		cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
+		account = cursor.fetchone()
+		if account:
+			msg = 'Account already exists !'
+		elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+			msg = 'Invalid email address !'
+		elif not re.match(r'[A-Za-z0-9]+', username):
+			msg = 'Username must contain only characters and numbers !'
+		elif not username or not password or not email:
+			msg = 'Please fill out the form !'
+		else:
+			cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email, ))
+			mysql.connection.commit()
+			msg = 'You have successfully registered !'
+	elif request.method == 'POST':
+		msg = 'Please fill out the form !'
+	return render_template('register.html', msg = msg)
+
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return chatbot_response(userText)
